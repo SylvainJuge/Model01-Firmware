1bc6846ebdb207e1152b6f62e3c476c9f2df9021
Sylvain Juge <syl20j@gmail.com>
2020-04-17 21:32:30 +0200

    reformat & cleanup

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -3,14 +3,8 @@
 // See "LICENSE" for license details
 
 #ifndef BUILD_INFORMATION
-#define BUILD_INFORMATION "locally built"
+#define BUILD_INFORMATION "sylvain"
 #endif
-
-
-/**
- * These #include directives pull in the Kaleidoscope firmware core,
- * as well as the Kaleidoscope plugins we use in the Model 01's firmware
- */
 
 
 // The Kaleidoscope core
@@ -137,31 +131,7 @@ enum { MACRO_VERSION_INFO,
   * Similarly, a key defined as 'LockLayer(NUMPAD)' will switch to NUMPAD when tapped.
   */
 
-/**
-  * Layers are "0-indexed" -- That is the first one is layer 0. The second one is layer 1.
-  * The third one is layer 2.
-  * This 'enum' lets us use names like QWERTY, FUNCTION, and NUMPAD in place of
-  * the numbers 0, 1 and 2.
-  *
-  */
-
 enum { PRIMARY, NUMPAD, FUNCTION }; // layers
-
-
-/**
-  * To change your keyboard's layout from QWERTY to DVORAK or COLEMAK, comment out the line
-  *
-  * #define PRIMARY_KEYMAP_QWERTY
-  *
-  * by changing it to
-  *
-  * // #define PRIMARY_KEYMAP_QWERTY
-  *
-  * Then uncomment the line corresponding to the layout you want to use.
-  *
-  */
-
-#define PRIMARY_KEYMAP_QWERTY
 
 // *INDENT-OFF*
 
@@ -169,55 +139,67 @@ KEYMAPS(
 
 // primary layer
 
-  [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
-
-   M(MACRO_TOGGLE_QUKEYS),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
+  [PRIMARY] = KEYMAP_STACKED(
+    ___,           Key_1,  Key_2,  Key_3,  Key_4,  Key_5,  Key_LEDEffectNext,
+    Key_Backtick,  Key_Q,  Key_W,  Key_E,  Key_R,  Key_T,  Key_Tab,
+    Key_PageUp,    Key_A,  Key_S,  Key_D,  Key_F,  Key_G, /*---*/
+    Key_PageDown,  Key_Z,  Key_X,  Key_C,  Key_V,  Key_B,  Key_Escape,
+    //
+    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+    //
+    ShiftToLayer(FUNCTION),
+    //============================================================================================
+    M(MACRO_TOGGLE_QUKEYS),  Key_6,  Key_7,     Key_8,      Key_9,       Key_0,          LockLayer(NUMPAD),
+    Key_Enter,               Key_Y,  Key_U,     Key_I,      Key_O,       Key_P,          Key_Equals,
+    /*---*/                  Key_H,  SFT_T(J),  Key_K,      Key_L,       Key_Semicolon,  Key_Quote,
+    Key_RightAlt,            Key_N,  Key_M,     Key_Comma,  Key_Period,  Key_Slash,      Key_Minus,
+    //
+    Key_RightShift,  Key_LeftAlt,  Key_Spacebar,  Key_RightControl,
+    //
+    ShiftToLayer(FUNCTION)),
 
 // numpad
 
-  [NUMPAD] =  KEYMAP_STACKED
-  (___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___,
-   ___,
-
-   M(MACRO_VERSION_INFO),  ___, Key_7, Key_8,      Key_9,              Key_KeypadSubtract, ___,
-   ___,                    ___, Key_4, Key_5,      Key_6,              Key_KeypadAdd,      ___,
-                           ___, Key_1, Key_2,      Key_3,              Key_Equals,         ___,
-   ___,                    ___, Key_0, Key_Period, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
-   ___, ___, ___, ___,
-   ___),
+  [NUMPAD] =  KEYMAP_STACKED(
+    ___,  ___,  ___,  ___,  ___,  ___,  ___,
+    ___,  ___,  ___,  ___,  ___,  ___,  ___,
+    ___,  ___,  ___,  ___,  ___,  ___,  /*---*/
+    ___,  ___,  ___,  ___,  ___,  ___,  ___,
+    //
+    ___, ___, ___, ___,
+    //
+    ___,
+    //============================================================================================
+    M(MACRO_VERSION_INFO),  ___,  Key_7,  Key_8,       Key_9,               Key_KeypadSubtract,  ___,
+    ___,                    ___,  Key_4,  Key_5,       Key_6,               Key_KeypadAdd,       ___,
+    /*---*/                 ___,  Key_1,  Key_2,       Key_3,               Key_Equals,          ___,
+    ___,                    ___,  Key_0,  Key_Period,  Key_KeypadMultiply,  Key_KeypadDivide,    Key_Enter,
+    //
+    ___, ___, ___, ___,
+    //
+    ___),
 
 // fn layer
 
-  [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   ___, Key_Delete, ___, ___,
+  [FUNCTION] =  KEYMAP_STACKED(
+    ___,       Key_F1,           Key_F2,       Key_F3,      Key_F4,         Key_F5,            Key_CapsLock,
+    Key_Tab,   ___,              Key_mouseUp,  ___,         Key_mouseBtnR,  Key_mouseWarpEnd,  Key_mouseWarpNE,
+    Key_Home,  Key_mouseL,       Key_mouseDn,  Key_mouseR,  Key_mouseBtnL,  Key_mouseWarpNW,   /*---*/
+    Key_End,   Key_PrintScreen,  Key_Insert,   ___,         Key_mouseBtnM,  Key_mouseWarpSW,   Key_mouseWarpSE,
+    //
+    ___,  Key_Delete,  ___,  ___,
+    //
    ___,
-
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ___, ___, Key_Enter, ___,
-   ___)
-) // KEYMAPS(
+    //============================================================================================
+    Consumer_ScanPreviousTrack,  Key_F6,                  Key_F7,                    Key_F8,                    Key_F9,           Key_F10,           Key_F11,
+    Consumer_PlaySlashPause,     Consumer_ScanNextTrack,  Key_LeftCurlyBracket,      Key_RightCurlyBracket,     Key_LeftBracket,  Key_RightBracket,  Key_F12,
+    /*---*/                      Key_LeftArrow,           Key_DownArrow,             Key_UpArrow,               Key_RightArrow,   ___,               ___,
+    Key_PcApplication,           Consumer_Mute,           Consumer_VolumeDecrement,  Consumer_VolumeIncrement,  ___,              Key_Backslash,     Key_Pipe,
+    //
+    ___,  ___,  Key_Enter,  ___,
+    //
+    ___)
+) // KEYMAPS
 
 /* Re-enable astyle's indent enforcement */
 // *INDENT-ON*
