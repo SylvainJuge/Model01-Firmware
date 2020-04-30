cc7fd977f46067a6f8022e029f8ee7810b28fa99
Sylvain Juge <syl20j@gmail.com>
2020-04-30 22:59:13 +0200

    remove mouse keys on fn layer

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -63,10 +63,10 @@ KEYMAPS(
 
 // fn layer
   [FUNCTION] =  KEYMAP_STACKED(
-    ___,       Key_F1,           Key_F2,       Key_F3,      Key_F4,         Key_F5,            Key_CapsLock,
-    Key_Tab,   ___,              Key_mouseUp,  ___,         Key_mouseBtnR,  Key_mouseWarpEnd,  Key_mouseWarpNE,
-    Key_Home,  Key_mouseL,       Key_mouseDn,  Key_mouseR,  Key_mouseBtnL,  Key_mouseWarpNW,   /*---*/
-    Key_End,   Key_PrintScreen,  Key_Insert,   ___,         Key_mouseBtnM,  Key_mouseWarpSW,   Key_mouseWarpSE,
+    ___,       Key_F1,           Key_F2,      Key_F3,  Key_F4,  Key_F5,  Key_CapsLock,
+    Key_Tab,   ___,              ___,         ___,     ___,     ___,     ___,
+    Key_Home,  ___,              ___,         ___,     ___,     ___,     /*---*/
+    Key_End,   Key_PrintScreen,  Key_Insert,  ___,     ___,     ___,     ___,
     //
     ___,  Key_Delete,  ___,  ___,
     //
